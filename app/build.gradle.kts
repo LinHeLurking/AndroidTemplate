57f74fd285ed00cbabfb76a96e5a868958a0a88e
LinHeLurking <LinHe.Lurking@gmail.com>
2023-04-04 18:39:38 +0800

    chores: update deps;
    Signed-off-by: LinHeLurking <LinHe.Lurking@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,7 +7,6 @@ fun cleanString(s: String): String {
     return s.removeSurrounding("\"").removeSurrounding("'")
 }
 
-val composeUiVersion = "1.3.1"
 val group = cleanString(properties["project.group"]!! as String)
 val nameLower = cleanString(properties["project.nameLower"]!! as String)
 val namePascal = cleanString(properties["project.namePascalCase"]!! as String)
@@ -27,8 +26,6 @@ android {
         vectorDrawables {
             useSupportLibrary = true
         }
-        manifestPlaceholders["projectGroup"] = group
-        manifestPlaceholders["projectNameLowerCase"] = nameLower
     }
 
     buildTypes {
@@ -53,7 +50,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.3.2"
+        kotlinCompilerExtensionVersion = "1.4.4"
     }
     packagingOptions {
         resources {
@@ -64,15 +61,15 @@ android {
 
 dependencies {
     implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.5.1")
-    implementation("androidx.activity:activity-compose:1.6.1")
-    implementation("androidx.compose.ui:ui:$composeUiVersion")
-    implementation("androidx.compose.ui:ui-tooling-preview:$composeUiVersion")
-    implementation("androidx.compose.material:material:1.3.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
+    implementation("androidx.activity:activity-compose:1.7.0")
+    implementation("androidx.compose.ui:ui:1.4.0")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.4.0")
+    implementation("androidx.compose.material:material:1.4.0")
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.4")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.0")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:$composeUiVersion")
-    debugImplementation("androidx.compose.ui:ui-tooling:$composeUiVersion")
-    debugImplementation("androidx.compose.ui:ui-test-manifest:$composeUiVersion")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.4.0")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.4.0")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:1.4.0")
 }
